c940f3832a6e82529e5605c1efe276bcb10ac1d1
Mob-y <serinteddy@gmail.com>
2026-02-21 20:02:52 +0100

    j'en peux plus

--- src/services/quest.service.js
+++ src/services/quest.service.js
@@ -1,17 +1,19 @@
+// src/services/quest.service.js
 const API_URL = import.meta.env.VITE_API_URL + "/api";
 console.log("API URL =", import.meta.env.VITE_API_URL);
-export const questsService = {
-	async getProgress() {
-		const response = await fetch(`${API_URL}/quests/progress`);
-		return await response.json();
-	},
 
-	async validateQuest(questId, answer) {
-		const response = await fetch(`${API_URL}/quests/${questId}/validate`, {
-			method: "POST",
-			headers: { "Content-Type": "application/json" },
-			body: JSON.stringify({ answer }),
-		});
-		return await response.json();
-	},
+export const questsService = {
+  async getProgress() {
+    const response = await fetch(`${API_URL}/quests/progress`);
+    return await response.json();
+  },
+
+  async validateQuest(questId, answer) {
+    const response = await fetch(`${API_URL}/quests/${questId}/validate`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ answer }),
+    });
+    return await response.json();
+  },
 };
