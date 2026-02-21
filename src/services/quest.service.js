86a8a3a2a70008ffd0b03dce99bf4891fa442e0f
Mob-y <serinteddy@gmail.com>
2026-02-21 19:05:30 +0100

    add env front

--- src/services/quest.service.js
+++ src/services/quest.service.js
@@ -1,5 +1,5 @@
 const API_URL = import.meta.env.VITE_API_URL + "/api";
-
+console.log("API URL =", import.meta.env.VITE_API_URL);
 export const questsService = {
 	async getProgress() {
 		const response = await fetch(`${API_URL}/quests/progress`);
